2089c5e3af9ba9636126722d52b06c96ae624124
Biel <vgabriel@id.uff.br>
2020-12-14 11:51:16 -0300

    Versão final do desafio;

--- js/main.js
+++ js/main.js
@@ -18,55 +18,186 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-// $('.buttonHilda').hover(function() {
-//     $('.selectedMovie').addClass('hilda');
-// }, function() {
-//     $('.selectedMovie').removeClass('hilda');
-// });
+let olderMovie = $('.selectedMovie');
 
-// $('#buttonHilda').click(function() {
-//     $('.selectedMovie').removeClass('sense8');
-//     $('.selectedMovie').addClass('hilda');
-// });
+let nameMovie = $('.selectMovieName');
+let olderName;
 
-// $('#buttonSense8').click(function() {
-//     $('.selectedMovie').removeClass('hilda');
-//     $('.selectedMovie').addClass('sense8');
-// });
+let descri = $('.selectMovieDescription');
+let olderDescrip;
 
+let clicked = false;
+
+let listOfClassesOfMyElement;
+
+//hover hilda
+$('.buttonHilda').hover(function() {/*if aqui */
+    listOfClassesOfMyElement = $(olderMovie).attr('class').split(/\s+/);
+
+    olderDescrip = descri.html();
+    olderName = nameMovie.html();
+    console.log(olderName);
+    olderMovie.removeClass();
+    nameMovie.html("Hilda");
+    descri.html("Ao sair da floresta onde vive e ir para a cidade, a destemida Hilda vive aventuras incríveis com os novos amigos e criaturas mágicas que encontra pelo caminho.");
+    olderMovie.addClass('selectedMovie');
+    olderMovie.addClass('hilda');
+
+}, function() { /*else aqui */
+    if(clicked == false && listOfClassesOfMyElement[1] != "hilda")
+    {
+        olderMovie.removeClass('hilda');
+        olderMovie.addClass(listOfClassesOfMyElement[1]);
+        nameMovie.html(olderName);
+        descri.html(olderDescrip);
+    }
+    clicked = false;
+});
+
+//hover carmen
+$('.buttonCarmen').hover(function() {/*if aqui */
+    listOfClassesOfMyElement = $(olderMovie).attr('class').split(/\s+/);
+
+    olderDescrip = descri.html();
+    olderName = nameMovie.html();
+    console.log(olderName);
+    olderMovie.removeClass();
+    nameMovie.html("Carmen Sandiego");
+    descri.html("A superladra Carmen Sandiego roda o mundo para frustrar os planos malignos da academia V.I.L.E., com a ajuda de seus engenhosos companheiros.");
+    olderMovie.addClass('selectedMovie');
+    olderMovie.addClass('carmen');
+
+}, function() { /*else aqui */
+    if(clicked == false && listOfClassesOfMyElement[1] != "carmen")
+    {
+        olderMovie.removeClass('carmen');
+        olderMovie.addClass(listOfClassesOfMyElement[1]);
+        nameMovie.html(olderName);
+        descri.html(olderDescrip);
+    }
+    clicked = false;
+});
+
+//hover 3 la embaixo
+$('.button3embaixo').hover(function() {/*if aqui */
+    listOfClassesOfMyElement = $(olderMovie).attr('class').split(/\s+/);
+
+    olderDescrip = descri.html();
+    olderName = nameMovie.html();
+    console.log(olderName);
+    olderMovie.removeClass();
+    nameMovie.html("Os 3 Lá Embaixo: Contos da Arcadia");
+        descri.html("Ao chegarem à Terra, dois jovens alienígenas em fuga tentam se infiltrar entre humanos e escapar de caçadores de recompensa intergaláticos.");
+    olderMovie.addClass('selectedMovie');
+    olderMovie.addClass('laEmbaixo');
+
+}, function() { /*else aqui */
+    if(clicked == false && listOfClassesOfMyElement[1] != "laEmbaixo")
+    {
+        olderMovie.removeClass('laEmbaixo');
+        olderMovie.addClass(listOfClassesOfMyElement[1]);
+        nameMovie.html(olderName);
+        descri.html(olderDescrip);
+    }
+    clicked = false;
+});
+
+//hover final space
+$('.buttonSpace').hover(function() {/*if aqui */
+    listOfClassesOfMyElement = $(olderMovie).attr('class').split(/\s+/);
+
+    olderDescrip = descri.html();
+    olderName = nameMovie.html();
+    console.log(olderName);
+    olderMovie.removeClass();
+    nameMovie.html("Final Space");
+    descri.html("Um humano prisioneiro no espaço e um incrível alienígena destruidor de planetas partem em uma aventura interestelar para salvar o universo.");
+    olderMovie.addClass('selectedMovie');
+    olderMovie.addClass('space');
+
+}, function() { /*else aqui */
+    if(clicked == false && listOfClassesOfMyElement[1] != "space")
+    {
+        olderMovie.removeClass('space');
+        olderMovie.addClass(listOfClassesOfMyElement[1]);
+        nameMovie.html(olderName);
+        descri.html(olderDescrip);
+    }
+    clicked = false;
+});
+
+//hover sense8
+$('.buttonSense').hover(function() {/*if aqui */
+    listOfClassesOfMyElement = $(olderMovie).attr('class').split(/\s+/);
+
+    olderDescrip = descri.html();
+    olderName = nameMovie.html();
+    console.log(olderName);
+    olderMovie.removeClass();
+    nameMovie.html("Sense8");
+    descri.html("Das criadoras de “Matrix” e “Babylon 5”, esta série segue oito desconhecidos que passam a compartilhar sentimentos e habilidades enquanto tentam evitar seu extermínio.");
+    olderMovie.addClass('selectedMovie');
+    olderMovie.addClass('sense8');
+
+}, function() { /*else aqui */
+    if(clicked == false && listOfClassesOfMyElement[1] != "sense8")
+    {
+        olderMovie.removeClass('sense8');
+        olderMovie.addClass(listOfClassesOfMyElement[1]);
+        nameMovie.html(olderName);
+        descri.html(olderDescrip);
+    }
+    clicked = false;
+});
 
 $(document).ready(function() {
      
     $('.buttonHilda').on('click', function(event){
-      var $other =  $('.selectedMovie');
-        $('.selectedMovie').removeClass();
-        $other.addClass('selectedMovie');
-        $other.addClass('hilda');
+        clicked = true;
+        olderMovie.removeClass();
+        olderMovie.addClass('selectedMovie');
+        olderMovie.addClass('hilda');
+        nameMovie.html("Hilda");
+        descri.html("Ao sair da floresta onde vive e ir para a cidade, a destemida Hilda vive aventuras incríveis com os novos amigos e criaturas mágicas que encontra pelo caminho.");
     });
     $('.buttonCarmen').on('click', function(event){
-        var $other =  $('.selectedMovie');
-        $('.selectedMovie').removeClass();
-        $other.addClass('selectedMovie');
-        $other.addClass('carmen');
+        clicked = true;
+        olderMovie.removeClass();
+        olderMovie.addClass('selectedMovie');
+        olderMovie.addClass('carmen');
+
+        nameMovie.html("Carmen Sandiego");
+        descri.html("A superladra Carmen Sandiego roda o mundo para frustrar os planos malignos da academia V.I.L.E., com a ajuda de seus engenhosos companheiros.");
     })
     $('.button3embaixo').on('click', function(event){
-        var $other =  $('.selectedMovie');
-        $('.selectedMovie').removeClass();
-        $other.addClass('selectedMovie');
-        $other.addClass('laEmbaixo');
+        clicked = true;
+
+        olderMovie.removeClass();
+        olderMovie.addClass('selectedMovie');
+        olderMovie.addClass('laEmbaixo');
+
+        nameMovie.html("Os 3 Lá Embaixo: Contos da Arcadia");
+        descri.html("Ao chegarem à Terra, dois jovens alienígenas em fuga tentam se infiltrar entre humanos e escapar de caçadores de recompensa intergaláticos.");
     });
     $('.buttonSpace').on('click', function(event){
-        var $other =  $('.selectedMovie');
-        $('.selectedMovie').removeClass();
-        $other.addClass('selectedMovie');
-        $other.addClass('space');
+        clicked = true;
+
+        olderMovie.removeClass();
+        olderMovie.addClass('selectedMovie');
+        olderMovie.addClass('space');
+        nameMovie.html("Final Space");
+        descri.html("Um humano prisioneiro no espaço e um incrível alienígena destruidor de planetas partem em uma aventura interestelar para salvar o universo.");
     });
 
     $('.buttonSense8').on('click', function(event){
-        var $other =  $('.selectedMovie');
-          $('.selectedMovie').removeClass();
-          $other.addClass('selectedMovie');
-          $other.addClass('sense8');
+        clicked = true;
+
+        olderMovie.removeClass();
+        olderMovie.addClass('selectedMovie');
+        olderMovie.addClass('sense8');
+
+        nameMovie.html("Sense8");
+        descri.html("Das criadoras de “Matrix” e “Babylon 5”, esta série segue oito desconhecidos que passam a compartilhar sentimentos e habilidades enquanto tentam evitar seu extermínio.");
       });
 
 
